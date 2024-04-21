63c8813c9a6f489748244c54cd6f14e78aa3d02e
Batt <abbilginn@hotmail.com>
2024-04-21 04:48:21 +0300

    Commit
    Force

--- Assets/Scripts/DisasterManager.cs
+++ Assets/Scripts/DisasterManager.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class DisasterManager : MonoBehaviour
 {
+    // 0 -> Solar Storm
+    // 1 -> Earthquak
+    // 2 -> Plague
+    // 3 -> Electricity Failure
     [SerializeField] private string[] disasterTypes;
+    [SerializeField] private CinemachineVirtualCamera mainCamera;
 
     public bool isDisasterHappening = false;
+    public int disasterIndex = -1;
+
+    private float timer = 0f;
+    private CinemachineBasicMultiChannelPerlin noise;
+   
+
 
     //Solar
     [Header("Solar Variables")]
@@ -17,54 +30,147 @@ public class DisasterManager : MonoBehaviour
     [SerializeField] private Image sunFilter;
     [SerializeField] private Transform goToPosSun;
     [SerializeField] private ParticleSystem sunParticles;
+    [SerializeField] private Transform initialSunPos;
 
-    private void Awake(){ DOTween.Init(); }
+
+
+
+    private void Awake()
+    {
+        DOTween.Init();
+        
+    }
 
     private void Start()
     {
-        DisasterMake(NextDisasterDecide());
+
+       
     }
 
-    private string NextDisasterDecide()
+    private void Update()
     {
-        int index = Random.Range(0, disasterTypes.Length);
-        return disasterTypes[index];   
-    }
-
-
-    private void SolarStorm()
-    {
-
-
-
-    }
-
-
-    private void DisasterMake(string disaster)
-    {
-        switch(disaster) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            SolarStormEnd();
+            StopCameraShake();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            SolarStorm();
+            ShakeScreen();
+        }
 
-            case "Solar":
+     
 
-                break;
+        timer += Time.deltaTime;
+        if (timer > 5f)
+        {
+            CallDisaster(NextDisasterDecide());
+            isDisasterHappening = true;
+            timer = 0f;
 
-            case "Earthquake":
-                break;
-
-            case "Freeze":
-                break;
-            case "Sand Storm":
-                break;
-
-            case "Plague":
-                break;
+            Debug.Log("Disaster is happening");
 
         }
 
 
     }
-    
+
+    //Earthquake
+    private void ShakeScreen()
+    {
+
+        noise =mainCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        if (noise)
+        {
+            noise.m_AmplitudeGain = 2f;
+            noise.m_FrequencyGain = 35f;
+        }
+
+    }
+
+    private void StopCameraShake()
+    {
+        noise =mainCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        if (noise)
+        {
+            noise.m_AmplitudeGain = 0f;
+            disasterIndex = -1;
+        }
+
+    }
+
+    private void CallDisaster(string disasterName)
+    {
+        switch (disasterName)
+        {
+            case "Solar":
+                SolarStorm();
+                disasterIndex = 0;
+                break;
+
+            case "Earthquake":
+                disasterIndex = 1;
+
+                break;
+
+            case "Plague":
+
+                break;
+
+            case "Electricity":
+
+                break;
+
+        }
+
+    }
+
+    private string NextDisasterDecide()
+    {
+        int index = Random.Range(0, disasterTypes.Length);
+        return disasterTypes[index];
+    }
+
+
+    private void SolarStorm()
+    {
+        sun.transform.DOMove(goToPosSun.position, 3f, false);
+        sun.transform.DOScale(1.5f, 1f);
+        sunParticles.startSpeed = 50f;
+        sunParticles.maxParticles = 3000;
+        sunFilter.gameObject.SetActive(true);
+        StartCoroutine(nameof(ChangeFade));
+        Debug.Log("Its storming");
+    }
+
+    private void SolarStormEnd()
+    {
+
+        sun.transform.DOMove(initialSunPos.position, 1f);
+        sun.transform.DOScale(1f, 1f);
+        sunFilter.gameObject.SetActive(false);
+        sunParticles.startSpeed = 5f;
+        sunParticles.maxParticles = 1000;
+        StopCoroutine(nameof(ChangeFade));
+        disasterIndex = -1;
+        Debug.Log("Solar is storm is resolved.");
+
+    }
+
+    private IEnumerator ChangeFade()
+    {
+        while (true)
+        {
+            sunFilter.GetComponent<Image>().DOFade(0.15f, 2f);
+            yield return new WaitForSeconds(2.5f);
+            sunFilter.GetComponent<Image>().DOFade(0.5f, 0f);
+        }
+
+    }
+
+     
+
 
 
 }
--- Assets/Scripts/InventorySc.cs
+++ Assets/Scripts/InventorySc.cs
@@ -14,89 +14,110 @@ public class InventorySc : MonoBehaviour
     public Button InventoryButton;
     public Button[] EnvButtons;
     public Sprite[] EnvButtonImage;
-    public static bool Envfull=false;
-    [SerializeField] private TMP_Text[] invCountText;
-    public void AddItem(Sprite ItemImage,int WhichItem)
+    public static bool Envfull = false;
+     public void AddItem(Sprite ItemImage, int WhichItem)
     {
-        int Counter=0;
+        int Counter = 0;
 
-        for(int i=0;i<EnvButtons.Length;i++)
+        for (int i = 0; i < EnvButtons.Length; i++)
         {
-           
-            if(EnvButtonImage[i]==null)
+
+            if (EnvButtonImage[i] == null)
             {
 
-                EnvButtonImage[i]=ItemImage;
+                EnvButtonImage[i] = ItemImage;
 
                 EnvButtons[i].GetComponent<ButtonItem>().WhichItem = WhichItem;
 
-                EnvButtons[i].GetComponent<Image>().sprite=EnvButtonImage[i];
+                EnvButtons[i].GetComponent<Image>().sprite = EnvButtonImage[i];
 
-                EnvButtons[i].GetComponent<Image>().DOFade(1,0);
+                EnvButtons[i].GetComponent<Image>().DOFade(1, 0);
 
                 break;
             }
             else
             {
-                Counter+=1;
+                Counter += 1;
 
-                if(Counter==4)
+                if (Counter == 4)
                 {
-                    Envfull=true;
+                    Envfull = true;
                 }
             }
         }
     }
     public void InventoryOpen()
     {
-        switch(Open)
+        switch (Open)
         {
-            case false: InventoryPanel.transform.DOLocalMoveX(-756,0.5f).SetEase(Ease.OutElastic); InventoryButton.transform.DORotate(new Vector3(0,0,0),0.3f); break;
-            case true: InventoryPanel.transform.DOLocalMoveX(-1163,0.5f).SetEase(Ease.OutElastic); InventoryButton.transform.DORotate(new Vector3(0,0,180),0.3f);break;
+            case false: InventoryPanel.transform.DOLocalMoveX(-756, 0.5f).SetEase(Ease.OutElastic); InventoryButton.transform.DORotate(new Vector3(0, 0, 0), 0.3f); break;
+            case true: InventoryPanel.transform.DOLocalMoveX(-1163, 0.5f).SetEase(Ease.OutElastic); InventoryButton.transform.DORotate(new Vector3(0, 0, 180), 0.3f); break;
         }
-       Open=!Open;
+        Open = !Open;
     }
     public void DeleteItem(int i)
     {
-        switch(i)
-        {
-            case 0: 
-                EnvButtonImage[0]=null;
-                EnvButtons[0].GetComponent<Image>().sprite=null; 
-                  EnvButtons[0].GetComponent<ButtonItem>().WhichItem=5; 
-              
-                invCountText[0].text = "0";
-                EnvButtons[0].GetComponent<Image>().DOFade(0.1f,0); 
-                
-                break;
-            case 1: 
-                EnvButtonImage[1]=null;
-                EnvButtons[1].GetComponent<Image>().sprite=null; 
-              
-                invCountText[1].text = "0";
-                EnvButtons[1].GetComponent<Image>().DOFade(0.1f,0); 
-                EnvButtons[1].GetComponent<ButtonItem>().WhichItem=5; 
-                break;
-            case 2: 
-                EnvButtonImage[2]=null;
-                EnvButtons[2].GetComponent<Image>().sprite=null; 
-                
-                invCountText[2].text = "0";
-                EnvButtons[2].GetComponent<Image>().DOFade(0.1f,0);
-                EnvButtons[2].GetComponent<ButtonItem>().WhichItem=5;  
-                break;
-            case 3: 
-                EnvButtonImage[3]=null;
-                EnvButtons[3].GetComponent<Image>().sprite=null; 
-                EnvButtons[3].GetComponent<ButtonItem>().WhichItem=5; 
-               
-                invCountText[3].text = "0";
-                EnvButtons[3].GetComponent<Image>().DOFade(0.1f,0); 
-                break;
-        }
-        Envfull=false;
+ 
+
+            switch (i)
+            {
+
+
+                case 0:
+
+                    if (EnvButtons[0].GetComponent<ButtonItem>().WhichItem != 5)
+                    {
+                        EnvButtonImage[0] = null;
+                        EnvButtons[0].GetComponent<Image>().sprite = null;
+                        EnvButtons[0].GetComponent<ButtonItem>().WhichItem = 5;
+                        
+                        EnvButtons[0].GetComponent<Image>().DOFade(0.1f, 0);
+
+                    }
+
+                    break;
+                case 1:
+                    if (EnvButtons[1].GetComponent<ButtonItem>().WhichItem != 5)
+                    {
+
+                        EnvButtonImage[1] = null;
+                        EnvButtons[1].GetComponent<Image>().sprite = null;
+                      
+                        EnvButtons[1].GetComponent<Image>().DOFade(0.1f, 0);
+                        EnvButtons[1].GetComponent<ButtonItem>().WhichItem = 5;
+
+                    }
+                    break;
+                case 2:
+                    if (EnvButtons[2].GetComponent<ButtonItem>().WhichItem != 5)
+                    {
+
+                        EnvButtonImage[2] = null;
+                        EnvButtons[2].GetComponent<Image>().sprite = null;
+                         
+                        EnvButtons[2].GetComponent<Image>().DOFade(0.1f, 0);
+                        EnvButtons[2].GetComponent<ButtonItem>().WhichItem = 5;
+                    }
+
+                    break;
+
+                case 3:
+                    if (EnvButtons[3].GetComponent<ButtonItem>().WhichItem != 5)
+                    {
+                        EnvButtonImage[3] = null;
+                        EnvButtons[3].GetComponent<Image>().sprite = null;
+                        EnvButtons[3].GetComponent<ButtonItem>().WhichItem = 5;
+                       
+                        EnvButtons[3].GetComponent<Image>().DOFade(0.1f, 0);
+
+                    }
+                    break;
+
+            }
+
+        Envfull = false;
     }
     // inventoryde Bloklara sprite şeklinde koydum datayı yani if statement i sprite a ve b ise çalıştır gibi olabilir bu spritelar EnvButtonImagede(buttonların içindeki sprite da)
     // bu button sprite ları buttonların numarasıyla aynı sırada buttonımage[1] ve button[1] aynı buttonu işaret ediyor gibi düşünebilirsin
-    
+
 }
